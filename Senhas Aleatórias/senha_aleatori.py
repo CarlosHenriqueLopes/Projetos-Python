ed43e489afaa7cdac089f24d4b3ab4a33b3799cc
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2022-08-12 23:37:14 -0300

    update

---
+++ Senhas Aleatórias/senha_aleatori.py
@@ -0,0 +1,8 @@
+import string
+
+letras = string.ascii_letters # -> pegas todas as letras da lib
+pontuação = string.punctuation # -> pegar toda as pontuações das lib
+letra_pont = letras + pontuação # -> contatenação
+
+print(letras)
+print(pontuação)
