04f4f8885a24ebb8b7635499fe087c8a0c40b9ad
CarlosHenriqueLopes <carloshenriquelopes362@gmail.com>
2022-08-13 11:05:07 -0300

    update

--- Senhas Aleatórias/senha_aleatori.py
+++ Senhas Aleatórias/senha_aleatori.py
@@ -1,8 +1,24 @@
 import string
+from random import SystemRandom
+
+# random.choices(sequence, weights=None, cum_weights=None, k=1)
+
+# sequence
+# Requeridos. Uma sequência como uma lista, uma tupla, um intervalo de números etc.
+
+# weights
+# Opcional. Uma lista onde você pode pesar a possibilidade para cada valor.
+# Padrão Nenhum
+
+# cum_weights
+# Opcional. Uma lista onde você pode pesar a possibilidade para cada valor, só que desta vez a possibilidade é acumulada.
+# Exemplo: lista de pesos normais: [2, 1, 1] é o mesmo que esta lista cum_weights; [2, 3, 4].
+# Padrão Nenhum
 
 letras = string.ascii_letters # -> pegas todas as letras da lib
 pontuação = string.punctuation # -> pegar toda as pontuações das lib
 letra_pont = letras + pontuação # -> contatenação
 
-print(letras)
-print(pontuação)
+senha = ''.join(SystemRandom().choices(letra_pont, k=6)) # -> k quantidade de numeros
+
+print(senha)
