60fc1dfa42066df41fb00d9317609554ba3d01db
CarlosHenriqueLopes <109255033+CarlosHenriqueLopes@users.noreply.github.com>
2023-05-11 00:36:33 -0300

    Add files via upload

---
+++ Flask/bd.py
@@ -0,0 +1,20 @@
+users = {
+    "Michel": {
+        "name": "michel j fox",
+        "tel": "99999",
+        "quotes": {
+            1: "Primeira frase Michel 01",
+            2: "Segunda frase Michel 02"
+        }
+    },
+
+    "david": {
+        "name": "david gueta",
+        "tel": "999999990",
+        "quotes": {
+            1: "Primeira frase David 01",
+            2: "Segunda frase David 02"
+        }
+    }
+
+}
